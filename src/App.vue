43ce98ef5aafd0c3b8c58c7976d5c2bda464b6f6
cegle <cegle@qq.com>
2018-12-17 17:15:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,75 @@
 <template>
-  <div id="app">
-    <div class="header">
-      <div class="logo"></div>
-    </div>
-    <div class="main">
-      <el-table
-        v-loading="loading"
-        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
-        style="width: 100%"
-      >
-        <el-table-column label="域名列表" prop="name"></el-table-column>
-        <el-table-column label="价格">
-          <template slot-scope="scope">
-            <el-tag>{{scope.row.price}}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="备注" align="center">
-          <template slot="header" slot-scope="scope">
-            <el-input v-model="search" size="mini" placeholder="输入域名关键字搜索"/>
-          </template>
-          <template slot-scope="scope">{{scope.row.desc}}</template>
-        </el-table-column>
-      </el-table>
-    </div>
-  </div>
+	<div id="app">
+		<div class="header">
+			<div class="logo">Domains</div>
+		</div>
+		<div class="main">
+			<el-table
+				v-loading="loading"
+				:data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+				style="width: 100%"
+			>
+				<el-table-column label="域名列表" prop="name"></el-table-column>
+				<el-table-column label="价格">
+					<template slot-scope="scope">
+						<el-tag>{{scope.row.price}}</el-tag>
+					</template>
+				</el-table-column>
+				<el-table-column label="备注" align="center">
+					<template slot="header" slot-scope="scope">
+						<el-input v-model="search" size="mini" placeholder="输入域名关键字搜索"/>
+					</template>
+					<template slot-scope="scope">{{scope.row.desc}}</template>
+				</el-table-column>
+			</el-table>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      tableData: [
-        {
-          name: "acg.tc",
-          price: "100",
-          desc: "acg"
-        },
-        {
-          name: "vpsbird.com",
-          price: "5000",
-          desc: "搞VPS的"
-        },
-        {
-          name: "2016-05-01",
-          price: "王小虎",
-          desc: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          name: "2016-05-03",
-          price: "王小虎",
-          desc: "上海市普陀区金沙江路 1516 弄"
-        }
-      ],
-      loading: false,
-      search: ""
-    };
-  },
-  methods: {}
-};
+	export default {
+		name: "App",
+		data() {
+			return {
+				tableData: [],
+				loading: true,
+				search: ""
+			};
+		},
+		methods: {},
+		mounted() {
+			this.$http
+				.get("../static/data/domainsData.json")
+				.then(res => {
+					// console.log(res);
+					if (res.status === 200) {
+						this.tableData = res.data;
+						this.loading = false;
+					}
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
 #app {
-  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
-    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
-  .header {
-    height: 120px;
-    border: 1px solid red;
-  }
-  .main {
-    max-width: 860px;
-    margin: 0 auto;
-  }
+	font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+		"Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+	.header {
+		height: 90px;
+		// border: 1px solid red;
+		text-align: center;
+		.logo {
+			font-size: 32px;
+			line-height: 90px;
+		}
+	}
+	.main {
+		max-width: 860px;
+		margin: 0 auto;
+	}
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
 
-Vue.use(ElementUI)
+Vue.prototype.$http = axios;
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
